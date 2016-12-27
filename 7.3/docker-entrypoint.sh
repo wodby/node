c4fc53127f7e45a463d62e43490ccd13c9cdb994
Pavel Prischepa <pprischepa@gmail.com>
2016-12-27 19:53:24 +0700

    Initial commit

--- 7.3/docker-entrypoint.sh
+++ 7.3/docker-entrypoint.sh
@@ -6,10 +6,4 @@ if [[ ! -z $DEBUG ]]; then
   set -x
 fi
 
-if [[ ! -z $WWW_DATA_GID && ! -z $WWW_DATA_UID ]]; then
-    deluser www-data
-    addgroup -g $WWW_DATA_GID -S www-data
-    adduser -u $WWW_DATA_UID -D -S -G www-data www-data
-fi
-
 exec node
