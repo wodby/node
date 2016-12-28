d4fc0e240edbff330c449e0c34e076c474970b32
Pavel Prischepa <pprischepa@gmail.com>
2016-12-28 14:02:30 +0700

    Added docker-entrypoint-init.d dir

--- 7.3/docker-entrypoint.sh
+++ 7.3/docker-entrypoint.sh
@@ -6,4 +6,15 @@ if [[ ! -z $DEBUG ]]; then
   set -x
 fi
 
+# Execute files in /docker-entrypoint-init.d
+shopt -s nullglob
+
+for f in /docker-entrypoint-init.d/*; do
+    case "$f" in
+        *.sh)   echo "$0: Running $f"; . "$f" ;;
+        *)      echo "$0: Ignoring $f" ;;
+    esac
+    echo
+done
+
 exec node
